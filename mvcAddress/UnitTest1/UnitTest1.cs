55ab37aa913866dd02ba5fe2453bddae7b9825f5
paulojakepaz@gmail.com <paulojakepaz@gmail.com>
2019-11-23 11:33:19 -0400

    prueba unitaria post examplk

---
+++ mvcAddress/UnitTest1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPostContact()
+        {
+            //Organizar
+
+
+            //Actuar   
+             
+            //Afirmar
+        }
+    }
+}
---
+++ mvcAddress/apiAddressbookTests/Controllers/BooksControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using apiAddressbook.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiAddressbook.Controllers.Tests
+{
+    [TestClass()]
+    public class BooksControllerTests
+    {
+        [TestMethod()]
+        public void PostBookTest()
+        {
+            var controller = new BooksController();
+            controller.Request= new HttpRequestMessage();
+            controller.Configuracion = new HttpConfiguracion();
+            IhttpActionResult actionresult= controller.PostBook(new Models)
+
+        }
+    }
+}
